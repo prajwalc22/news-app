635d096530abb06ee5457fcef8e17bd3ab9a5a0b
prajwalc22 <prajwalchoudhari22@gmail.com>
2024-06-10 11:59:22 +0530

    Hello

---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import NewsItem from "./NewsItem";
+import React, { useState } from "react";
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(
+          "https://newsapi.org/v2/top-headlines?country=us&apiKey=593cebeb813c438db162bc09a2a8dd08"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setArticles(data.articles);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewsBoard;
---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,28 @@
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
+      style={{ maxWidth: "345px" }}
+    >
+      {src && (
+        <img
+          src={src}
+          style={{ height: "200px", width: "328px" }}
+          className="card-img-top"
+          alt="News"
+        />
+      )}
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description ? description.slice(0, 90) : "hello"}
+        </p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
